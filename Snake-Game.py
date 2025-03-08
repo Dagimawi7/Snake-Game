2b2a146dde8e656b1f72b863d61b52497ec65bdc
Dagimawi7 <20dtamrat@gmail.com>
2025-03-08 17:08:28 -0500

    Initialize project with Pygame setup and core configurations

---
+++ Snake-Game.py
@@ -0,0 +1,19 @@
+# setting up the libraries 
+import pygame
+import time 
+import random 
+
+# speed of the snake
+Speed_Of_Snake = 15
+
+# size of the window 
+Size_Window_x = 720
+Size_Window_y = 480
+
+# Important colors 
+black = pygame.Color(0, 0, 0)
+white = pygame.Color(255, 255, 255)
+red = pygame.Color(255, 0, 0)
+green = pygame.Color(0, 255, 0)
+blue = pygame.Color(0, 0, 255)
+
