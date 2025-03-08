506dd8743f9ee7fe9fa01e6da0d163b5cf814cdf
Dagimawi7 <20dtamrat@gmail.com>
2025-03-08 18:05:51 -0500

    Implement main game loop with movement, collision detection, and scoring

--- Snake-Game.py
+++ Snake-Game.py
@@ -78,3 +78,85 @@ def Game_Over():
     game_window.blit(Game_Over_Surface, Game_Over_Rect)
     pygame.display.flip()
     
+    # game will quit after 3 seconds
+    time.sleep(3)
+
+    # shutting off pygame
+    pygame.quit()
+    # shutting off the program
+    quit()
+
+    # Main function 
+    while True:
+        # dealing with key events 
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    change_to = 'UP'
+                if event.key == pygame.K_DOWN:
+                    change_to = 'DOWN'
+                if event.key == pygame.K_LEFT:
+                    change_to = 'LEFT'
+                if event.key == pygame.K_RIGHT:
+                    change_to = 'RIGHT'
+        
+        # If I press 2 keys together. I don't want to move 2 dierctions together
+        if change_to == 'UP' and Direction != 'DOWN':
+            Direction = 'UP'
+        if change_to == 'DOWN' and Direction != 'UP':
+            Direction = 'DOWN'
+        if change_to == 'LEFT' and Direction != 'RIGHT':
+            Direction = 'LEFT'
+        if change_to == 'RIGHT' and Direction != 'LEFT':
+            Direction = 'RIGHT'
+        
+        # moving the snake
+        if Direction == 'UP':
+            Postion_Of_Snake[1] -= 10
+        if Direction == 'DOWN':
+            Postion_Of_Snake[1] += 10
+        if Direction == 'LEFT':
+            Postion_Of_Snake[0] -= 10
+        if Direction == 'RIGHT':
+            Postion_Of_Snake[0] += 10
+        
+        # mechanism for growing the snakes body, if snake eats fruit socre +10
+        Body_Of_Snake.insert(0, list(Postion_Of_Snake))
+    if Postion_Of_Snake[0] == Position_of_Fruit[0] and Postion_Of_Snake[1] == Position_of_Fruit[1]:
+        Score += 10
+        Spawn_Fruit = False
+    else:
+        Body_Of_Snake.pop()
+        
+    if not Spawn_Fruit:
+        Position_of_Fruit = [random.randrange(1, (Size_Window_x//10)) * 10, 
+                          random.randrange(1, (Size_Window_y//10)) * 10]
+        
+    Spawn_Fruit = True
+    game_window.fill(black)
+    
+    for pos in Body_Of_Snake:
+        pygame.draw.rect(game_window, green, pygame.Rect(
+          pos[0], pos[1], 10, 10))
+        
+    pygame.draw.rect(game_window, white, pygame.Rect(
+      Position_of_Fruit[0], Position_of_Fruit[1], 10, 10))
+
+    # condtions to end the game 
+    if Postion_Of_Snake[0] < 0 or Postion_Of_Snake[0] > Size_Window_x-10:
+        game_over()
+    if Postion_Of_Snake[1] < 0 or Postion_Of_Snake[1] > Size_Window_y-10:
+        game_over()
+    # Touching the snakes body 
+    for block in Body_Of_Snake[1:]:
+        if Postion_Of_Snake[0] == block[0] and Postion_Of_Snake[1] == block[1]:
+            game_over()
+    
+    # displaying score continuously
+    Show_Score(1, white, 'times new roman', 20)
+    
+    # Refresh game screen
+    pygame.display.update()
+
+    # Frame Per Second /Refresh Rate
+    fps.tick(Speed_Of_Snake)
